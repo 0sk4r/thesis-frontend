acc87fd051ba09462f178de6ed344a0f5579ccd4
Oskar Sobczyk <sobczykoskar@gmail.com>
2019-08-28 18:27:49 +0200

    add link in comment

--- src/components/Comment/CommentShow.jsx
+++ src/components/Comment/CommentShow.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import {Comment} from 'antd';
+import { Comment } from "antd";
+import { Link } from "react-router-dom";
 import moment from "moment";
 
 // Componetn display single comment. Receive data in props
 function CommentShow(props) {
-  const {comment} = props;
+  const { comment } = props;
+
   return (
     <Comment
       id={comment.id}
-      author={comment.user.name}
+      author={
+        <Link to={`/users/${comment.user.id}`}>{comment.user.nickname}</Link>
+      }
       avatar={comment.user.image.url}
       content={comment.content}
       datetime={moment(comment.created_at).fromNow()}
     />
-  )
+  );
 }
 
 export default CommentShow;
